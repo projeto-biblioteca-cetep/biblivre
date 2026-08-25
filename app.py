c94ed896971df05cda7a306ee1cd146234eaf245
Projeto Biblioteca <projetobibliotecacetep@gmail.com>
2026-08-25 11:14:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -448,4 +448,4 @@ def reportar_bug():
     return jsonify({"status": "sucesso"}), 201
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=False, port=5001)
